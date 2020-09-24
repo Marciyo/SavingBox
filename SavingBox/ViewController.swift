d931ca6c6a9314cc6689e1e5b726188e702fcaf6
Marcel Mierzejewski <marcelmierzejewski@gmail.com>
2020-09-24 16:41:23 +0100

    Initial Commit

---
+++ SavingBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SavingBox
+//
+//  Created by Marcel Mierzejewski on 24/09/2020.
+//  Copyright © 2020 Marcel Mierzejewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
